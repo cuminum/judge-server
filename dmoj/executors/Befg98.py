359d77e00186b556a17a233625264bded34debe1
cuminum <88176718+cuminum@users.noreply.github.com>
2025-06-07 02:10:24 +0900

    add befunge98

---
+++ dmoj/executors/Befg98.py
@@ -0,0 +1,9 @@
+from dmoj.executors.script_executor import ScriptExecutor
+
+
+class Executor(ScriptExecutor):
+    ext = 'befunge98'
+    command = 'befunge98'
+    test_program = """\
+"!dlroW ,olleH :ohce",,,,,,,,,,,,,,,,,,,@
+"""
