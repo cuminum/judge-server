89624b3c3c63ad05c7fc5c3763c58799241c3d35
cuminum <88176718+cuminum@users.noreply.github.com>
2025-06-08 19:25:58 +0900

    add whitespace

---
+++ dmoj/executors/WSPACE.py
@@ -0,0 +1,50 @@
+from dmoj.executors.script_executor import ScriptExecutor
+
+
+class Executor(ScriptExecutor):
+    ext = 'ws'
+    command = 'whitespace'
+    test_program = """\
+   		  	 	
+	
+     		   		
+	
+     		 	   
+	
+     		 				
+	
+     			 	 
+	
+     	     
+	
+     	  	   
+	
+     		  	 	
+	
+     		 		  
+	
+     		 		  
+	
+     		 				
+	
+     	 		  
+	
+     	     
+	
+     	 	 			
+	
+     		 				
+	
+     			  	 
+	
+     		 		  
+	
+     		  	  
+	
+     	    	
+	
+  
+
+
+\
+"""
